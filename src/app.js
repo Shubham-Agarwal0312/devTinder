9e3373c2130d2a921c97c48430609c2f9d322131
Shubham <shubhamagarwal0312@gmail.com>
2024-11-18 08:49:00 +0530

    exploring multiple route handler for one request

--- src/app.js
+++ src/app.js
@@ -2,14 +2,26 @@ const express = require('express');
 
 const app = express();
 
-app.get('/user/:id/:name/:age', (req, res) => {
-    console.log(req.query);
-    console.log(req.params);
-    res.send({
-        firstName: 'Shubham',
-        lastname: 'Agarwal'
-    });
-});
+app.get('/user', (req, res, next) => {
+    console.log('Request Handler 1');
+    next();
+    // res.send('Response 1');
+}, (req, res, next) => {
+    console.log('Request Handler 2');
+    // res.send('Response 2');
+    next();
+    console.log('Request Handler 2.1');
+    res.send('Response 2');
+})
+
+// app.get('/user/:id/:name/:age', (req, res) => {
+//     console.log(req.query);
+//     console.log(req.params);
+//     res.send({
+//         firstName: 'Shubham',
+//         lastname: 'Agarwal'
+//     });
+// });
 
 // app.get('/user', (req, res) => {
 //     res.send({
@@ -26,9 +38,9 @@ app.get('/user/:id/:name/:age', (req, res) => {
 //     res.send("User Data successfully deleted from the DB");
 // });
 
-app.use((req, res) => {
-    res.send('This is dashboard for devTinder');
-});
+// app.use((req, res) => {
+//     res.send('This is dashboard for devTinder');
+// });
 
 app.listen(3000, () => {
     console.log('server started successfully');
